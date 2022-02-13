d58b0936eae2ea80aeccb1975ed92cd5e321e121
sumaiyatasnim <tasnimsumaiya07@gmail.com>
2022-02-13 16:02:22 +0600

    updated get and post features

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+app.use(express.json());
 
 //mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ugc3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,6 +21,64 @@ async function run() {
         //the client is mongodb here. upore j client ache oitake connect korar jonne ekhane wait korbo.database connect hoye gelo.
         await client.connect();
         // console.log('database connected successfully');
+        const database = client.db('wildFlowerPottery');
+        const productCollection = database.collection('products');
+        const myOrderCollection = database.collection('myOrder');
+        const reviewCollection = database.collection('review');
+        const usersCollection = database.collection('users');
+
+        // find operation
+        // get all products api. express er get lagbe.
+        app.get('/allProducts', async (req, res) => {
+            //product ta find korbo
+            const cursor = productCollection.find({});
+            //await korbo karon cursor ta te ja ja pawa geche shob array te convert korbo.shob gula array k diye dbe
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        //get single service
+        app.get('/allProducts/:id', async (req, res) => {
+            //req object er moddhe params er moddhe id ta pawa jay
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log('id', id);
+            const product = await productCollection.findOne(query);
+            res.json(product);
+        });
+        //My Orders Post API
+        app.post('/myOrders', async (req, res) => {
+            const myOrder = req.body;
+            const result = await myOrderCollection.insertOne(myOrder);
+            res.json(result);
+            console.log(result);
+        })
+
+        //My order get api
+        app.get("/myOrder/:email", async (req, res) => {
+            const result = await myOrderCollection
+                .find({
+                    email: req.params.email
+                })
+                .toArray();
+            res.send(result);
+        });
+        /// delete order
+        app.delete("/delteOrder/:id", async (req, res) => {
+            const result = await myOrderCollection.deleteOne({
+                _id: ObjectId(req.params.id),
+            });
+            res.send(result);
+        });
+        //Post api
+        app.post('/addProducts', async (req, res) => {
+            //jei request ta pathano hocche shei product ta pabo req.body theke.
+            const products = req.body;
+            // console.log('hit the post api', products);
+            const result = await productCollection.insertOne(products);
+            console.log(result);
+
+            res.json(result)
+        })
     }
     finally {
         //jokhon shob kaj hoye jabe tokhon she close korar cheshta korbe
